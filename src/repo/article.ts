de7497cc3f03ab96eb29b74fe9d0ef4d47d1bb48
Shahabuddin Ahmed <shahabuddin.cse.ru@gmail.com>
2023-03-01 23:20:16 +0600

    user and article repos are implemented

---
+++ src/repo/article.ts
@@ -0,0 +1,107 @@
+import { Types } from "mongoose";
+import { CommentModel, CommentInterface } from "./../model/comment";
+import { LikeModel, LikeInterface } from "./../model/like";
+import { ArticleInterface, ArticleModel } from "../model/article";
+import { SortType } from "../model/user";
+
+export interface ArticleRepoInterface {
+  create(article: ArticleInterface): Promise<ArticleInterface>;
+  get(skip: number, limit: number, sort: SortType): Promise<ArticleInterface[]>;
+  getById(articleId: string): Promise<ArticleInterface | null>;
+  leaveComment(
+    userId: string,
+    articleId: string,
+    content: string
+  ): Promise<CommentInterface>;
+  giveLike(
+    userId: string,
+    articleId: string,
+    isLike: boolean
+  ): Promise<LikeInterface>;
+  count(userId: string): Promise<number>;
+}
+
+export class ArticleRepo implements ArticleRepoInterface {
+  constructor(
+    public articleModel: typeof ArticleModel,
+    public commentModel: typeof CommentModel,
+    public likeModel: typeof LikeModel
+  ) {
+    this.articleModel = articleModel;
+    this.commentModel = commentModel;
+    this.likeModel = likeModel;
+  }
+
+  public async create(article: ArticleInterface): Promise<ArticleInterface> {
+    return this.articleModel.create(article);
+  }
+
+  public async get(
+    skip: number,
+    limit: number,
+    sort: SortType
+  ): Promise<ArticleInterface[]> {
+    return this.articleModel.find(
+      {},
+      { content: -1, user: -1 },
+      { skip, limit, sort: { createdAt: sort === SortType.ASC ? 1 : -1 } }
+    );
+  }
+
+  public async getById(articleId: string): Promise<ArticleInterface | null> {
+    return this.articleModel.findById({ _id: articleId }).populate("category");
+  }
+
+  public async count(): Promise<number> {
+    return this.articleModel.count();
+  }
+
+  public async leaveComment(
+    userId: string,
+    articleId: string,
+    content: string
+  ): Promise<CommentInterface> {
+    const checkExistingComment = await this.getCommentByArticleId(articleId);
+    if (!checkExistingComment) {
+      const parentId = new Types.ObjectId();
+      return this.commentModel.create({
+        _id: parentId,
+        userId,
+        articleId,
+        parentId,
+        content,
+      });
+    }
+
+    return this.commentModel.create({
+      userId,
+      articleId,
+      parentId: checkExistingComment.id,
+      content,
+    });
+  }
+
+  public async getCommentByArticleId(
+    articleId: string
+  ): Promise<ArticleInterface> {
+    return this.commentModel.findOne({ articleId }).populate("category");
+  }
+
+  public async giveLike(
+    userId: string,
+    articleId: string,
+    isLike: boolean
+  ): Promise<LikeInterface> {
+    return this.likeModel.create({ userId, articleId, isLike });
+  }
+}
+
+export const newArticleRepo = async (
+  articleModel: typeof ArticleModel,
+  commentModel: typeof CommentModel,
+  likeModel: typeof LikeModel
+): Promise<ArticleRepoInterface> => {
+  return new ArticleRepo(articleModel, commentModel, likeModel);
+};
+
+export default ArticleRepo;
